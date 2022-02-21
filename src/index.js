b3f01465c997a703266701cf002606f720543c14
blindLegi <alekse.skiruha@gmail.com>
2022-02-21 15:12:47 +0300

    fear: complete toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+let oneDigit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let twoDigit = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let manyDigit = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+
+
+const underHundred = (number) => {
+    if (number < 10) {
+        return oneDigit[number];
+    } else if (number < 20) {
+        return twoDigit[number - 10];
+    } else if (number < 100) {
+        return `${manyDigit[Math.floor((number / 10) - 2)]}${(number%10 == 0) ? '' : ' ' + oneDigit[number%10]}`;
+    }
+}
+
 module.exports = function toReadable (number) {
-  
+   if (number < 100) {
+      return underHundred(number); 
+   } else {
+       return `${oneDigit[Math.floor(number / 100)]} hundred${(number%100 == 0) ? '' : ' ' + underHundred(number%100)}`
+   }
 }
